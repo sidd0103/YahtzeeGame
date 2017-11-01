b05c386c6355e6120fc7aeed548d701996dd4512
sidd0103 <sidbiyer@gmail.com>
2017-11-01 10:47:05 -0700

    storage completly working

--- src/YahtzeeGame.java
+++ src/YahtzeeGame.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.stream.*;
 public class YahtzeeGame {
 	private ScoreBoard gameScoreBoard = new ScoreBoard();
-	private YahtzeeDice gameDice = new YahtzeeDice();
+	private YahtzeeDice liveDice = new YahtzeeDice();
 	private int gameScore; 
 	private int turn = 1;
 	
@@ -12,14 +12,14 @@ public class YahtzeeGame {
 	}
 	public void run() {
 		System.out.println("Welcome to Yahtzee! ");
-		System.out.println("This is a dice game where you can roll dice or take them");
-		System.out.println("To get specific combinations of di!");
+		System.out.println("This is a dice game where you can roll dice or store them");
+		System.out.println("to get specific combinations of di!");
 		System.out.println("---------");
 		boolean scoreNotFull = true;
 		while (scoreNotFull) {
 			//This while loop keeps track of the turns taking place
 			//these are turn based variables. 
-			gameDice.setNumDie(5);
+			liveDice.setNumDie(5);
 			int[] storedDiValues = new int[5];
 			int indexToAddToStoredValues = 0;
 			
@@ -30,15 +30,20 @@ public class YahtzeeGame {
 				System.out.println("");
 				System.out.println("Roll # " + (4-i));
 				if (i > 2) {
-					String choiceRoll = this.getChoiceRoll();
+					String choiceRoll = this.getValidChoice("roll");
 					doRoll();	
-					System.out.println("Stored values are: " + Arrays.toString(storedValuesFormatted(storedDiValues)));
+					int[] diVals = liveDice.getDiceValues();
+					int[] storedVals = getStoredValuesFormatted(storedDiValues);
+					int[] hand = getHand(diVals, storedVals);
+					System.out.println("These are the values of the di: " + Arrays.toString(diVals));
+					System.out.println("Stored values are: " + Arrays.toString(storedVals));
+					System.out.println("Your hand is: " + Arrays.toString(hand));
 				}
 				else {
-					String choiceRollorTake = getChoiceRollOrTake();
+					String choiceRollorTake = this.getValidChoice("roll","store");
 					if (choiceRollorTake.equals("store")) {
 						System.out.println("Which di would you like to take out? ");
-						int[] diVals = gameDice.getDiceValues();
+						int[] diVals = liveDice.getDiceValues();
 						int[] diNumberRepresentations = IntStream.rangeClosed(1, diVals.length).toArray();
 						System.out.println("These are the values of the di: " + Arrays.toString(diVals));
 						System.out.println("Use these numbers " + Arrays.toString(diNumberRepresentations) + " to choose a di");
@@ -54,7 +59,7 @@ public class YahtzeeGame {
 							//this part creates a pay load of values chosen 
 							for (int j = 0; j < chosenDi.length(); j ++) {
 								//make sure that we aren't choosing indexes that don't have a corresponding die
-								if (j > gameDice.getNumDie() - 1) {
+								if (j > liveDice.getNumDie() - 1) {
 									break;
 								}
 								else {
@@ -70,15 +75,21 @@ public class YahtzeeGame {
 								storedDiValues[indexToAddToStoredValues] = value;
 								indexToAddToStoredValues ++;
 							}
-							System.out.println("Stored values are: " + Arrays.toString(storedValuesFormatted(storedDiValues)));
-							gameDice.setNumDie(gameDice.getNumDie() - payload.length);
+							int[] storedVals = getStoredValuesFormatted(storedDiValues);
+							System.out.println("Stored values are: " + Arrays.toString(storedVals));
+							liveDice.setNumDie(liveDice.getNumDie() - payload.length);
 							break;
 						}
 		
 					}
 					else if (choiceRollorTake.equals("roll")) {
 						doRoll();
-						System.out.println("Stored values are: " + Arrays.toString(storedValuesFormatted(storedDiValues)));
+						int[] diVals = liveDice.getDiceValues();
+						int[] storedVals = getStoredValuesFormatted(storedDiValues);
+						int[] hand = getHand(diVals, storedVals);
+						System.out.println("These are the values of the di: " + Arrays.toString(diVals));
+						System.out.println("Stored values are: " + Arrays.toString(storedVals));
+						System.out.println("Your hand is: " + Arrays.toString(hand));
 					}
 				}
 				System.out.println("Roll completed");
@@ -86,37 +97,39 @@ public class YahtzeeGame {
 			this.turn ++;
 		}
 	}
-	private String getChoiceRoll() {
-		boolean goodChoice = false;
-		String choiceRoll = "";
-		while (goodChoice == false) {
-			System.out.print("Type roll to roll the di: ");
-			choiceRoll = TextIO.getlnString();
-			if (choiceRoll.toLowerCase().equals("roll")) {
-				goodChoice = true;
-			}
-		}
-		return choiceRoll;
-	}
-	private String getChoiceRollOrTake() {
-		boolean goodChoice = false;
-		String choiceRoll = "";
-		while (goodChoice == false) {
-			System.out.print("Type roll to roll the di or store to store the die: ");
-			choiceRoll = TextIO.getlnString();
-			if (choiceRoll.toLowerCase().equals("roll") || choiceRoll.toLowerCase().equals("store")) {
-				goodChoice = true;
-			}
-		}
-		return choiceRoll;
-	}
+	
+	//rolls the Yahtzee die object
 	private void doRoll() {
 		System.out.println("Rolling...");
-		int rollValue = gameDice.roll();
-		int[] diVals = gameDice.getDiceValues();
-		System.out.println("These are the values of the di: " + Arrays.toString(diVals));
+		liveDice.roll();
 	}
-	private int[] storedValuesFormatted(int[] storedDiValues) {
+	//get a valid choice
+	private String getValidChoice(String arg1) {
+		boolean goodChoice = false;
+		String choiceRoll = "";
+		while (goodChoice == false) {
+			System.out.print("Type "+arg1+" to "+arg1+" the di: ");
+			choiceRoll = TextIO.getlnString();
+			if (choiceRoll.toLowerCase().equals(arg1)) {
+				goodChoice = true;
+			}
+		}
+		return choiceRoll;
+	}
+	private String getValidChoice(String arg1, String arg2) {
+		boolean goodChoice = false;
+		String choiceRoll = "";
+		while (goodChoice == false) {
+			System.out.print("Type "+arg1+" to "+arg1+" the di or "+arg2+" to "+arg2+" the die: ");
+			choiceRoll = TextIO.getlnString();
+			if (choiceRoll.toLowerCase().equals(arg1) || choiceRoll.toLowerCase().equals(arg2)) {
+				goodChoice = true;
+			}
+		}
+		return choiceRoll;
+	}
+	//formats the stored values from this object by getting rid of all the 0's
+	private int[] getStoredValuesFormatted(int[] storedDiValues) {
 		int length = 0;
 		for (int value : storedDiValues) {
 			if (value != 0) {
@@ -130,6 +143,20 @@ public class YahtzeeGame {
 				returnArray[counter] = value;
 				counter ++;
 			}
+		}
+		return returnArray;
+	}
+	//combines the live dice values and the stored values to return the current hand
+	private int[] getHand(int[] rollValues, int[] formattedStoredDiValues) {
+		int[] returnArray = new int[rollValues.length + formattedStoredDiValues.length];
+		int index = 0;
+		for (int val : rollValues) {
+			returnArray[index] = val;
+			index ++;
+		}
+		for (int val : formattedStoredDiValues) {
+			returnArray[index] = val;
+			index ++;
 		}
 		return returnArray;
 	}
